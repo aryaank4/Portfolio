5575274545d72aff630a1a92fbe00d56e1959729
Aryaan Kharbade <aryaankharbade@Aryaans-MacBook-Air.local>
2023-11-13 22:46:17 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,188 @@
+import {
+  car,
+  python,
+  vscode,
+  mysql,
+  snapgram,
+  contact,
+  java,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  nextjs,
+  nodejs,
+  react,
+  summiz,
+  tailwindcss,
+  typescript,
+  camp,
+  dog,
+  promptopia,
+  nike,
+  dalle,
+  crypto
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: java,
+    name: "Java",
+    type: "Language",
+  },
+  {
+    imageUrl: python,
+    name: "Python",
+    type: "Language",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: vscode,
+    name: "VSCode",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: mysql,
+    name: "MySQL",
+    type: "Database",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/aryaank4",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/aryaan-kharbade-73660424b/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: crypto,
+    theme: "btn-back-blue",
+    name: "CryptoTransfer",
+    description:
+      "Users can transfer crypto from one account to another using metamask.",
+    link: "https://github.com/aryaank4/Krypt",
+  },
+  {
+    iconUrl: dalle,
+    theme: "btn-back-pink",
+    name: "Dall-E2.0",
+    description:
+      "A website where people can generate images by using prompt and can share it with community if they like.",
+    link: "https://github.com/aryaank4/dall-e",
+  },
+  {
+    iconUrl: nike,
+    theme: "btn-back-black",
+    name: "NikeSite",
+    description:
+      "A site where users can see products, Reviews, popular products, etc.",
+    link: "https://github.com/aryaank4/NikeSite",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "AI Summarizer Application",
+    description:
+      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+    link: "https://github.com/aryaank4/Sumz",
+  },
+  {
+    iconUrl: promptopia,
+    theme: "btn-back-yellow",
+    name: "Promptopia",
+    description:
+      "Users can create account and submit AI prompts so that other users can use it",
+    link: "https://github.com/aryaank4/project_promtopia",
+  },
+  {
+    iconUrl: dog,
+    theme: "btn-back-yellow",
+    name: "TinDog",
+    description: "For users who have dogs. Where dogs can meet.",
+    link: "https://github.com/aryaank4/TinDog",
+  },
+  {
+    iconUrl: camp,
+    theme: "btn-back-yellow",
+    name: "CampSite",
+    description: "A Landing Page where people can see places, reviews, etc.",
+    link: "https://github.com/aryaank4/CampSite",
+  },
+];
